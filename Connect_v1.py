02679e0a392a61d388c858aa3f2fa85b1167b9d1
Stanley Xu <stanley.xu@bcds.com.au>
2022-09-06 16:23:47 +1000

    Connect_v1.py

---
+++ Connect_v1.py
@@ -0,0 +1,62 @@
+# This code is a prototype script to establish basic communication with a SensThys RFID reader.
+import re
+import requests
+from config_files import*
+
+
+
+class reader_setup:
+    api_power = "/stapi/v0/ant/pwr"
+    api_inv = "/stapi/v0/inv/timed"
+
+    def build_endpoint(self, add_endpoint):
+        return self.base_Url + add_endpoint
+
+    def __init__(self,api_base_url, api_key,retries=3):
+        self.api_key = api_key
+        self.base_Url = api_base_url
+        self.retries = retries
+        self.headers = self.constructHeader()
+
+    def constructHeader(self):
+        headers = {
+            'Authorization': f'Bearer {self.api_key}',
+            'Content-Type': 'application/json;charset=utf-8'
+        }
+        return headers
+
+    def extract_info(self, add_endpoint):
+        r = requests.get(self.build_endpoint(add_endpoint), headers = self.headers)
+        return r
+
+    def attempt_connection(self):
+        self.retries = 3
+
+        for i in range(self.retries):
+            r = requests.get(self.base_Url)
+            if r.status_code == 200:
+                print("Connected")
+                return r
+            elif i == 2:
+                print("Connection to RFID reader failed")
+            else:
+                print(f"Connection could not be established. Attempt to reconnect try {i}")
+
+    def power(self):
+        return self.extract_info(reader_setup.api_power)
+    # Extracting the power of the reader
+
+
+    # Running a timed inventory 
+    def timed_inv(self):
+        return self.extract_info(reader_setup.api_inv)
+
+
+if __name__ == '__main__':
+    reader = reader_setup(api_base_url, api_key)
+    attempt = reader.attempt_connection()
+    r_power = reader.power()
+    r_inv = reader.timed_inv()
+   
+    print(r_power.text)
+    print(r_inv.text)
